c296457a46d71efe231845e2a4a52916fad5e6d2
nikolzerparivas-a11y <nikol.zerparivas@gmail.com>
2026-09-12 19:55:55 -0300

    Delete wdd131 directory

--- wdd131/scripts/obterdatas.js
+++
@@ -1,8 +0,0 @@
-const anoAtual = new Date().getFullYear();
-
-document.getElementById("anoatual").textContent = anoAtual;
-
-const ultimaModificacao = document.lastModified;
-
-document.getElementById("ultimaModificacao").textContent = `Última modificação: ${ultimaModificacao}`;
-
